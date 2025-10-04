61046bf89206b430a7553fd870207724728759aa
Bernardosm <bernardos2002@hotmail.com>
2025-10-04 13:12:46 -0300

    Initial Implementation (Nothing changed from original)

---
+++ addons/burnem/WBK_FlamethrowerSystem/config.cpp
@@ -0,0 +1,314 @@
+class CfgPatches 
+{
+	class WBK_PhoenixTreal_FlamethrowerMOD
+	{
+		units[] = { };
+		weapons[] = { };
+		requiredVersion = 1.620000;
+		requiredAddons[] = {
+				"CBA_Extended_EventHandlers", "CBA_MAIN"
+		};
+		author[] = {
+		};
+	};
+};
+
+
+class CfgSounds
+{
+    sounds[] = {}; 
+	class hit_by_flame_1
+    {
+    name = "hit_by_flame_1";
+    sound[] = {"\WBK_FlamethrowerSystem\flameThrowerSounds\hit_by_flame_1.wav", db+9, 1};
+    titles[]={};
+	};
+	class hit_by_flame_2
+    {
+    name = "hit_by_flame_2";
+    sound[] = {"\WBK_FlamethrowerSystem\flameThrowerSounds\hit_by_flame_2.wav", db+9, 1};
+    titles[]={};
+	};
+	class hit_by_flame_3
+    {
+    name = "hit_by_flame_3";
+    sound[] = {"\WBK_FlamethrowerSystem\flameThrowerSounds\hit_by_flame_3.wav", db+9, 1};
+    titles[]={};
+	};
+	class hit_by_flame_4
+    {
+    name = "hit_by_flame_4";
+    sound[] = {"\WBK_FlamethrowerSystem\flameThrowerSounds\hit_by_flame_4.wav", db+9, 1};
+    titles[]={};
+	};
+	class Bm_gas_leak
+    {
+    name = "Bm_gas_leak";
+    sound[] = {"\WBK_FlamethrowerSystem\flameThrowerSounds\gas_leak.ogg", db+10, 1};
+    titles[]={};
+	};
+	class Bm_fireFadeout
+    {
+    name = "Bm_fireFadeout";
+    sound[] = {"\WBK_FlamethrowerSystem\flameThrowerSounds\fire_ignite_2.wav", db+10, 1};
+    titles[]={};
+	};
+	class molotov_break_sound
+    {
+    name = "molotov_break_sound";
+    sound[] = {"\WBK_FlamethrowerSystem\flameThrowerSounds\molotov_detonate_3.wav", db+10, 1};
+    titles[]={};
+	};
+	class fuelTank_befexp
+    {
+    name = "fuelTank_befexp";
+    sound[] = {"\WBK_FlamethrowerSystem\flameThrowerSounds\fuelTank_befexp.ogg", db+10, 1};
+    titles[]={};
+	};
+	class fuelTank_befexp_1
+    {
+    name = "fuelTank_befexp_1";
+    sound[] = {"\WBK_FlamethrowerSystem\flameThrowerSounds\fuelTank_befexp_1.wav", db+10, 1};
+    titles[]={};
+	};
+	class flamethrower_start
+    {
+    name = "flamethrower_start";
+    sound[] = {"\WBK_FlamethrowerSystem\flameThrowerSounds\flamethrower_start.wav", db+3, 1};
+    titles[]={};
+	};
+	class flamethrower_end
+    {
+    name = "flamethrower_end";
+    sound[] = {"\WBK_FlamethrowerSystem\flameThrowerSounds\flamethrower_end.wav", db+3, 1};
+    titles[]={};
+	};
+	class flamethrower_looping
+    {
+    name = "flamethrower_looping";
+    sound[] = {"\WBK_FlamethrowerSystem\flameThrowerSounds\flamethrower_looping.wav", db+2.5, 1};
+    titles[]={};
+	};
+	
+	class flamewerfer_start
+    {
+    name = "flamewerfer_start";
+    sound[] = {"\WBK_FlamethrowerSystem\flameThrowerSounds\flamewerfer_start.wav", db+3, 1};
+    titles[]={};
+	};
+	class flamewerfer_end
+    {
+    name = "flamewerfer_end";
+    sound[] = {"\WBK_FlamethrowerSystem\flameThrowerSounds\flamewerfer_end.wav", db+3, 1};
+    titles[]={};
+	};
+	class flamewerfer_looping
+    {
+    name = "flamewerfer_looping";
+    sound[] = {"\WBK_FlamethrowerSystem\flameThrowerSounds\flamewerfer_looping.wav", db+2.5, 1};
+    titles[]={};
+	};
+	class flamethrower_tank_bulletimpact_01
+    {
+    name = "flamethrower_tank_bulletimpact_01";
+    sound[] = {"\WBK_FlamethrowerSystem\flameThrowerSounds\flamethrower_tank_bulletimpact_01.wav", db+9, 1};
+    titles[]={};
+	};
+	class flamethrower_tank_bulletimpact_02
+    {
+    name = "flamethrower_tank_bulletimpact_02";
+    sound[] = {"\WBK_FlamethrowerSystem\flameThrowerSounds\flamethrower_tank_bulletimpact_02.wav", db+9, 1};
+    titles[]={};
+	};
+	class flamethrower_tank_bulletimpact_03
+    {
+    name = "flamethrower_tank_bulletimpact_03";
+    sound[] = {"\WBK_FlamethrowerSystem\flameThrowerSounds\flamethrower_tank_bulletimpact_03.wav", db+9, 1};
+    titles[]={};
+	};
+	class flamethrower_tank_bulletimpact_04
+    {
+    name = "flamethrower_tank_bulletimpact_04";
+    sound[] = {"\WBK_FlamethrowerSystem\flameThrowerSounds\flamethrower_tank_bulletimpact_04.wav", db+9, 1};
+    titles[]={};
+	};
+	class flamethrower_tank_bulletimpact_05
+    {
+    name = "flamethrower_tank_bulletimpact_05";
+    sound[] = {"\WBK_FlamethrowerSystem\flameThrowerSounds\flamethrower_tank_bulletimpact_05.wav", db+9, 1};
+    titles[]={};
+	};
+	class run_flame_1
+    {
+    name = "run_flame_1";
+    sound[] = {"\WBK_FlamethrowerSystem\flameThrowerSounds\run_flame_1.wav", db+9, 1};
+    titles[]={};
+	};
+	class run_flame_2
+    {
+    name = "run_flame_2";
+    sound[] = {"\WBK_FlamethrowerSystem\flameThrowerSounds\run_flame_2.wav", db+9, 1};
+    titles[]={};
+	};
+	class run_flame_3
+    {
+    name = "run_flame_3";
+    sound[] = {"\WBK_FlamethrowerSystem\flameThrowerSounds\run_flame_3.wav", db+9, 1};
+    titles[]={};
+	};
+	class run_flame_4
+    {
+    name = "run_flame_4";
+    sound[] = {"\WBK_FlamethrowerSystem\flameThrowerSounds\run_flame_4.wav", db+9, 1};
+    titles[]={};
+	};
+	class run_flame_5
+    {
+    name = "run_flame_5";
+    sound[] = {"\WBK_FlamethrowerSystem\flameThrowerSounds\run_flame_5.wav", db+9, 1};
+    titles[]={};
+	};
+	class run_flame_6
+    {
+    name = "run_flame_6";
+    sound[] = {"\WBK_FlamethrowerSystem\flameThrowerSounds\run_flame_6.wav", db+9, 1};
+    titles[]={};
+	};
+	class igned_idle
+    {
+    name = "igned_idle";
+    sound[] = {"\WBK_FlamethrowerSystem\flameThrowerSounds\igned_idle.ogg", db+10, 1};
+    titles[]={};
+	};
+	class burnttodeath_distant1
+    {
+    name = "burnttodeath_distant1";
+    sound[] = {"\WBK_FlamethrowerSystem\flameThrowerSounds\burnttodeath_distant1.ogg", db+10, 1};
+    titles[]={};
+	};
+	class burnttodeath_distant2
+    {
+    name = "burnttodeath_distant2";
+    sound[] = {"\WBK_FlamethrowerSystem\flameThrowerSounds\burnttodeath_distant2.ogg", db+10, 1};
+    titles[]={};
+	};
+	class burnttodeath_distant3
+    {
+    name = "burnttodeath_distant3";
+    sound[] = {"\WBK_FlamethrowerSystem\flameThrowerSounds\burnttodeath_distant3.ogg", db+10, 1};
+    titles[]={};
+	};
+	class burnttodeath_distant4
+    {
+    name = "burnttodeath_distant4";
+    sound[] = {"\WBK_FlamethrowerSystem\flameThrowerSounds\burnttodeath_distant4.ogg", db+10, 1};
+    titles[]={};
+	};
+	class burnttodeath_distant5
+    {
+    name = "burnttodeath_distant5";
+    sound[] = {"\WBK_FlamethrowerSystem\flameThrowerSounds\burnttodeath_distant5.ogg", db+10, 1};
+    titles[]={};
+	};
+	class burnttodeath_distant6
+    {
+    name = "burnttodeath_distant6";
+    sound[] = {"\WBK_FlamethrowerSystem\flameThrowerSounds\burnttodeath_distant6.ogg", db+10, 1};
+    titles[]={};
+	};
+	class burnttodeath1
+    {
+    name = "burnttodeath1";
+    sound[] = {"\WBK_FlamethrowerSystem\flameThrowerSounds\burnttodeath1.ogg", db+10, 1};
+    titles[]={};
+	};
+	class burnttodeath2
+    {
+    name = "burnttodeath2";
+    sound[] = {"\WBK_FlamethrowerSystem\flameThrowerSounds\burnttodeath2.ogg", db+10, 1};
+    titles[]={};
+	};
+	class burnttodeath3
+    {
+    name = "burnttodeath3";
+    sound[] = {"\WBK_FlamethrowerSystem\flameThrowerSounds\burnttodeath3.ogg", db+10, 1};
+    titles[]={};
+	};
+	class burnttodeath4
+    {
+    name = "burnttodeath4";
+    sound[] = {"\WBK_FlamethrowerSystem\flameThrowerSounds\burnttodeath4.ogg", db+10, 1};
+    titles[]={};
+	};
+	class burnttodeath5
+    {
+    name = "burnttodeath5";
+    sound[] = {"\WBK_FlamethrowerSystem\flameThrowerSounds\burnttodeath5.ogg", db+10, 1};
+    titles[]={};
+	};
+	class burnttodeath6
+    {
+    name = "burnttodeath6";
+    sound[] = {"\WBK_FlamethrowerSystem\flameThrowerSounds\burnttodeath6.ogg", db+10, 1};
+    titles[]={};
+	};
+};
+
+
+class Extended_PreInit_EventHandlers
+{
+    class WBK_Flamethrower_PreInit
+    {
+        init="call compile preprocessFileLineNumbers '\WBK_FlamethrowerSystem\bootstrap\XEH_preInit.sqf'";
+    };
+};
+
+
+class Extended_PostInit_EventHandlers 
+{
+    class WBK_Flamethrower_PostInit 
+	{
+        init="call compile preprocessFileLineNumbers '\WBK_FlamethrowerSystem\bootstrap\XEH_postInit.sqf'";
+    };
+};
+
+
+class Extended_Killed_Eventhandlers
+{
+	class CAManBase
+	{
+		class FLAME_UNIT_DEATH
+		{
+			killed="[_this select 0] spawn Flame_Death_container_ArmaOne";
+		};
+	};
+};
+
+class Extended_Hit_Eventhandlers
+{
+	class CAManBase
+	{
+		class FLAME_UNIT_HIT
+		{
+			hit="[_this select 0, _this select 1] spawn Flame_Hit_container";
+		};
+	};
+};
+
+class Extended_Fired_Eventhandlers {
+    class CAManBase {
+        class Flamethrower_Fire_Class {
+            fired = "[_this select 0, _this select 1, _this select 2, _this select 3, _this select 4, _this select 5, _this select 6] spawn Flamethrower_Fired_EH";
+        };
+    };
+};
+
+
+class Extended_InitPost_EventHandlers {
+    class CAManBase {
+        class SW_Soldier_initMelee_eh {
+			onRespawn="true";
+            init = "_unit = _this select 0; if (local _unit) then {_unit setVariable [""IMS_SoundObject"",nil,true];};";
+        };
+    };
+};
