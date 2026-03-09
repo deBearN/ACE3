2be0f7e38c7d5fd44917692a7b3aadd0291b9ab5
Bernardosm <bernardos2002@hotmail.com>
2026-03-09 18:59:53 -0300

    Commented Debug stuff, should work

--- addons/burnem/script_component.hpp
+++ addons/burnem/script_component.hpp
@@ -2,17 +2,17 @@
 #define COMPONENT_BEAUTIFIED BurnEm
 #include "\z\ace\addons\main\script_mod.hpp"
 
-#define DEBUG_MODE_FULL
-// #define DISABLE_COMPILE_CACHE
-// #define ENABLE_PERFORMANCE_COUNTERS
+// #define DEBUG_MODE_FULL
+// // #define DISABLE_COMPILE_CACHE
+// // #define ENABLE_PERFORMANCE_COUNTERS
 
-#ifdef DEBUG_ENABLED_BURNEM
-    #define DEBUG_MODE_FULL
-#endif
+// #ifdef DEBUG_ENABLED_BURNEM
+//     #define DEBUG_MODE_FULL
+// #endif
 
-#ifdef DEBUG_SETTINGS_BURNEM
-    #define DEBUG_SETTINGS DEBUG_SETTINGS_BURNEM
-#endif
+// #ifdef DEBUG_SETTINGS_BURNEM
+//     #define DEBUG_SETTINGS DEBUG_SETTINGS_BURNEM
+// #endif
 
 #include "\z\ace\addons\main\script_macros.hpp"
 
